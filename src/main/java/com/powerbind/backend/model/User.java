8a044db4883771ec51910350e84ea6f479471705
nxstray <afwanapriansyah@gmail.com>
2026-09-03 12:22:16 +0700

    refactor: rename email field to username in User model

--- src/main/java/com/powerbind/backend/model/User.java
+++ src/main/java/com/powerbind/backend/model/User.java
@@ -22,7 +22,7 @@ public class User {
     private UUID id;
 
     @Column(nullable = false, unique = true)
-    private String email;
+    private String username;
 
     @Column(nullable = false)
     private String password;
