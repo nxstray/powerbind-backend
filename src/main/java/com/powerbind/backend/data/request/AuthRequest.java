7105060c60ef7f01e6f1c18e305fb9ef49b22dd4
nxstray <afwanapriansyah@gmail.com>
2026-09-03 12:23:43 +0700

    refactor: switch login request to username and drop Register class

--- src/main/java/com/powerbind/backend/data/request/AuthRequest.java
+++ src/main/java/com/powerbind/backend/data/request/AuthRequest.java
@@ -1,8 +1,6 @@
 package com.powerbind.backend.data.request;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,27 +9,11 @@ public class AuthRequest {
     @Getter
     @Setter
     public static class Login {
-        @NotBlank(message = "Email is required")
-        @Email(message = "Invalid email format")
-        private String email;
+        @NotBlank(message = "Username is required")
+        private String username;
 
         @NotBlank(message = "Password is required")
         private String password;
-    }
-
-    @Getter
-    @Setter
-    public static class Register {
-        @NotBlank(message = "Email is required")
-        @Email(message = "Invalid email format")
-        private String email;
-
-        @NotBlank(message = "Password is required")
-        @Size(min = 8, message = "Password must be at least 8 characters")
-        private String password;
-
-        @NotBlank(message = "Display name is required")
-        private String displayName;
     }
 
     @Getter
